06151dead30f4cf00226f42bfac628a6bda9733f
Samuel Roberts <rober352@purdue.edu>
2017-11-29 05:19:42 -0500

    Start scripting functionality in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -7,4 +7,33 @@ class Assignment {
         var description = description
         var name = name
     }
+
+}
+
+class AssignmentList {
+    constructor() {
+        this.assignments = []
+        this.classes = []
+        this.setupEventListeners()
+    }
+
+    setupEventListeners() {
+        document.querySelector('#add-assignment').addEventListener(this.handleAddAssignment.bind(this))
+    }
+
+    handleAddAssignment(ev) {
+        ev.preventDefault()
+        const f = ev.target
+
+        const assignment = {
+            course: f.course.value,
+            type: f.type.value,
+            dueDate: f.dueDate.value,
+            dueTime: f.dueTime.value,
+            description: f.description.value,
+            name: name
+        }
+
+        //save to stuff
+    }
 }
