f114d3913ca9b4729b41563cb2648ed9a547f666
Samuel Roberts <rober352@purdue.edu>
2017-11-28 20:18:14 -0500

    Add basic Assignment data structure

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+class Assignment {
+    constructor(course, type, dueDate, dueTime, description, name) {
+        var course = course
+        var type = type
+        var dueDate = dueDate
+        var dueTime = dueTime
+        var description = description
+        var name = name
+    }
+}
